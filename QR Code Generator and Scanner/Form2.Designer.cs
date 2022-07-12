3bb1bb96eabd71158dc5f4136b331b1d48bb9f29
Geoffrey Dela Rosa <delarosageoff@gmail.com>
2022-07-12 23:38:10 +0800

    Form 2

--- QR Code Generator and Scanner/Form2.Designer.cs
+++ QR Code Generator and Scanner/Form2.Designer.cs
@@ -28,19 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelIdentification = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxIdentification = new System.Windows.Forms.TextBox();
+            this.comboBoxDevice = new System.Windows.Forms.ComboBox();
+            this.pictureBoxCamera = new System.Windows.Forms.PictureBox();
+            this.labelCamera = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).BeginInit();
             this.SuspendLayout();
+            // 
+            // labelIdentification
+            // 
+            this.labelIdentification.AutoSize = true;
+            this.labelIdentification.Location = new System.Drawing.Point(456, 35);
+            this.labelIdentification.Name = "labelIdentification";
+            this.labelIdentification.Size = new System.Drawing.Size(77, 15);
+            this.labelIdentification.TabIndex = 13;
+            this.labelIdentification.Text = "Identification";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(344, 31);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(72, 23);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxIdentification
+            // 
+            this.textBoxIdentification.Location = new System.Drawing.Point(456, 72);
+            this.textBoxIdentification.Multiline = true;
+            this.textBoxIdentification.Name = "textBoxIdentification";
+            this.textBoxIdentification.Size = new System.Drawing.Size(308, 348);
+            this.textBoxIdentification.TabIndex = 11;
+            // 
+            // comboBoxDevice
+            // 
+            this.comboBoxDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDevice.FormattingEnabled = true;
+            this.comboBoxDevice.Location = new System.Drawing.Point(96, 31);
+            this.comboBoxDevice.Name = "comboBoxDevice";
+            this.comboBoxDevice.Size = new System.Drawing.Size(223, 23);
+            this.comboBoxDevice.TabIndex = 10;
+            // 
+            // pictureBoxCamera
+            // 
+            this.pictureBoxCamera.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxCamera.Location = new System.Drawing.Point(36, 72);
+            this.pictureBoxCamera.Name = "pictureBoxCamera";
+            this.pictureBoxCamera.Size = new System.Drawing.Size(404, 348);
+            this.pictureBoxCamera.TabIndex = 9;
+            this.pictureBoxCamera.TabStop = false;
+            // 
+            // labelCamera
+            // 
+            this.labelCamera.AutoSize = true;
+            this.labelCamera.Location = new System.Drawing.Point(36, 34);
+            this.labelCamera.Name = "labelCamera";
+            this.labelCamera.Size = new System.Drawing.Size(54, 15);
+            this.labelCamera.TabIndex = 8;
+            this.labelCamera.Text = "Camera :";
             // 
             // FormQRCode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelIdentification);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxIdentification);
+            this.Controls.Add(this.comboBoxDevice);
+            this.Controls.Add(this.pictureBoxCamera);
+            this.Controls.Add(this.labelCamera);
             this.Name = "FormQRCode";
             this.Text = "QR Code Scanner";
+            this.Load += new System.EventHandler(this.FormQRCode_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label labelIdentification;
+        private Button buttonStart;
+        private TextBox textBoxIdentification;
+        private ComboBox comboBoxDevice;
+        private PictureBox pictureBoxCamera;
+        private Label labelCamera;
     }
 }
--- QR Code Generator and Scanner/Form2.cs
+++ QR Code Generator and Scanner/Form2.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using ZXing.Aztec;
+
 
 namespace QR_Code_Generator_and_Scanner
 {
     public partial class FormQRCode : Form
     {
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
         public FormQRCode()
         {
             InitializeComponent();
+
+            FilterInfoCollection filterInfoCollection;
+            VideoCaptureDevice captureDevice;
         }
+
+        private void FormQRCode_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevice)
+                comboBoxDevice.Items.Add(Device.Name);
+
+            comboBoxDevice.SelectedIndex = 0;
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBoxDevice.SelectedIndex].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(CaptureDevice_NewFrame);
+            FinalFrame.Start();
+            
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBoxCamera.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+       
     }
 }
